63b024eb1d92a8fb754bfadcf32040ab2ec4d06f
areslp <areslp@gmail.com>
2012-11-23 09:08:21 +0800

    多特征lrr

--- multi_lrr.m
+++ multi_lrr.m
@@ -36,9 +36,9 @@ Z=zeros(3,n*n);
 % parameters
 mu=1e-6;
 max_mu=10^10;
-rho=1.1;
-epsilon=1e-6;
-epsilon2=1e-2;
+rho=1.9;
+epsilon=1e-4;
+epsilon2=1e-5; % must be small!
 
 % pre caculate matrix value
 xtx1=X1'*X1;
@@ -50,6 +50,11 @@ inv_x3=inv(xtx3+eye(n));
 X1f=norm(X1,'fro');
 X2f=norm(X2,'fro');
 X3f=norm(X3,'fro');
+
+
+changeX=0;
+changeZJ=0;
+changeZS=0;
 
 MAX_ITER=1000;
 iter=0;
@@ -64,9 +69,9 @@ while ~convergenced
     J1b=J1;
     J2b=J2;
     J3b=J3;
-    J1=singular_value_shrinkage(Z1+W1/mu,1/mu);
-    J2=singular_value_shrinkage(Z2+W2/mu,1/mu);
-    J3=singular_value_shrinkage(Z3+W3/mu,1/mu);
+    [J1,svp1]=singular_value_shrinkage(Z1+W1/mu,1/mu);
+    [J2,svp2]=singular_value_shrinkage(Z2+W2/mu,1/mu);
+    [J3,svp3]=singular_value_shrinkage(Z3+W3/mu,1/mu);
     % update S_i
     S1b=S1;
     S2b=S2;
@@ -80,6 +85,7 @@ while ~convergenced
     F2=(J2+S2-(W2+V2)*mu)/2;
     F3=(J3+S3-(W3+V3)*mu)/2;
     M=[reshape(F1',1,n*n);reshape(F2',1,n*n);reshape(F3',1,n*n)];
+
     Z=l21(M,alpha/(2*mu));
     % update Z_i
     Z1b=Z1;
@@ -92,9 +98,9 @@ while ~convergenced
     E1b=E1;
     E2b=E2;
     E3b=E3;
-    E1=l21(X1-X1*S1+Y1/mu,lambda/mu); 
-    E2=l21(X2-X2*S2+Y2/mu,lambda/mu); 
-    E3=l21(X3-X3*S3+Y3/mu,lambda/mu); 
+    E1=l21(X1-X1*S1+Y1/mu,lambda/(2*mu));  % bug fixed, parameter should be lambda/(2*mu), not lambda/mu
+    E2=l21(X2-X2*S2+Y2/mu,lambda/(2*mu)); 
+    E3=l21(X3-X3*S3+Y3/mu,lambda/(2*mu)); 
 
     % temporary matrix
     X1_c=X1-X1*S1-E1;
@@ -110,22 +116,33 @@ while ~convergenced
     ZS3_c=Z3-S3;
 
     % check convergence
-%     changeX=max(max(norm(X1_c,'fro')/X1f,norm(X2_c,'fro')/X2f),norm(X3_c,'fro')/X3f);
-%     changeZJ=max(max(norm(ZJ1_c,'fro')/X1f,norm(ZJ2_c,'fro')/X2f),norm(ZJ3_c,'fro')/X3f);
-%     changeZS=max(max(norm(ZS1_c,'fro')/X1f,norm(ZS2_c,'fro')/X2f),norm(ZS3_c,'fro')/X3f);
-    
     changeX=max(max(norm(X1_c,'fro')/X1f,norm(X2_c,'fro')/X2f),norm(X3_c,'fro')/X3f);
     changeZJ=max(max(norm(ZJ1_c,'fro')/X1f,norm(ZJ2_c,'fro')/X2f),norm(ZJ3_c,'fro')/X3f);
     changeZS=max(max(norm(ZS1_c,'fro')/X1f,norm(ZS2_c,'fro')/X2f),norm(ZS3_c,'fro')/X3f);
-    fprintf(1,'mu is %f,changeX is %f,changeZJ is %f,changeZS is %f\n',mu,changeX,changeZJ,changeZS);
-    if changeX <= epsilon && changeZJ <= epsilon && changeZS <= epsilon
-        convergenced=true;
-        fprintf(1,'convergenced, iter is %d\n',iter);
+
+    
+    changeZ=max(max(norm(Z1-Z1b,'fro')/X1f,norm(Z2-Z2b,'fro')/X2f),norm(Z3-Z3b,'fro')/X3f);
+    changeJ=max(max(norm(J1-J1b,'fro')/X1f,norm(J2-J2b,'fro')/X2f),norm(J3-J3b,'fro')/X3f);
+    changeS=max(max(norm(S1-S1b,'fro')/X1f,norm(S2-S2b,'fro')/X2f),norm(S3-S3b,'fro')/X3f);
+    changeE=max(max(norm(E1-E1b,'fro')/X1f,norm(E2-E2b,'fro')/X2f),norm(E3-E3b,'fro')/X3f);
+    tmp=[changeZ changeJ changeS changeE ];
+    gap=mu*max(tmp);
+
+    if mod(iter,50)==0
+        fprintf(1,'===========================================================================================================\n');
+        fprintf(1,'gap between two iteration is %f,mu is %f\n',gap,mu);
+        fprintf(1,'iter %d,mu is %f,ResidualX is %f,changeZJ is %f,changeZS is %f\n',iter,mu,changeX,changeZJ,changeZS);
+        fprintf(1,'svp1 %d,svp2 %d,svp3 %d\n',svp1,svp2,svp3);
     end
-    % if changeX <= epsilon
-        % convergenced=true;
-        % fprintf(1,'convergenced, iter is %d\n',iter);
-    % end
+
+
+    % if changeX <= epsilon && changeZJ <= epsilon && changeZS <= epsilon
+    if changeX <= epsilon && gap <=epsilon2 && changeZJ <= epsilon && changeZS <= epsilon
+        convergenced=true;
+        fprintf(2,'convergenced, iter is %d\n',iter);
+        fprintf(2,'iter %d,mu is %f,ResidualX is %f,changeZJ is %f,changeZS is %f\n',iter,mu,changeX,changeZJ,changeZS);
+        fprintf(2,'svp1 %d,svp2 %d,svp3 %d\n',svp1,svp2,svp3);
+    end
 
     % update multipliers
     Y1=Y1+mu*(X1_c);
@@ -140,17 +157,9 @@ while ~convergenced
     V2=V2+mu*(ZS2_c);
     V3=V3+mu*(ZS3_c);
     % update parameters
-    changeZ=max(max(norm(Z1-Z1b,'fro')/X1f,norm(Z2-Z2b,'fro')/X2f),norm(Z3-Z3b,'fro')/X3f);
-    changeJ=max(max(norm(J1-J1b,'fro')/X1f,norm(J2-J2b,'fro')/X2f),norm(J3-J3b,'fro')/X3f);
-    changeS=max(max(norm(S1-S1b,'fro')/X1f,norm(S2-S2b,'fro')/X2f),norm(S3-S3b,'fro')/X3f);
-    changeE=max(max(norm(E1-E1b,'fro')/X1f,norm(E2-E2b,'fro')/X2f),norm(E3-E3b,'fro')/X3f);
-
-    tmp=[changeZ changeJ changeS changeE];
-    fprintf(1,'gap between two iteration is %f\n',max(tmp));
-    if max(tmp) < epsilon2
+    if gap < epsilon2
         mu=min(rho*mu,max_mu);
     end
     
     iter=iter+1;
-    fprintf(1,'iter is %d\n',iter);
 end

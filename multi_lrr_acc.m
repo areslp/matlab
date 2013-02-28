2123f272029d93a7467432e4f9c1779f88d83137
Luo Pei <areslp@gmail.com>
2013-03-01 00:34:36 +0800

    speed up

--- multi_lrr_acc.m
+++ multi_lrr_acc.m
@@ -93,11 +93,16 @@ for i=1:k
 end
 F=cell(k,1);
 
+
+M=cell(k,1);
+
 MAX_ITER=1000;
 iter=0;
 convergenced=false;
 
 
+clambda=cell(k,1);
+clambda(1:k)={lambda};
 
 tic
 while ~convergenced
@@ -105,44 +110,17 @@ while ~convergenced
         fprintf(1,'max iter num reached!\n');
         break;
     end
-    % tic
-    % update J_i
-    for i=1:k
-        Jk{i}=J{i};
-        [JT,svpt,svt]=singular_value_shrinkage_acc(Z{i}+W{i}/mu,1/mu,sv{i});
-        % [JT,svpt]=singular_value_shrinkage(Z{i}+W{i}/mu,1/mu);
-        J{i}=JT;
-        svp{i}=svpt;
-        sv{i}=svt;
-    end
-    % t=toc;
-    % fprintf(1,'singular_value_shrinkage takes %f\n',t);
-    % update S_i
-    % tic
-    % for i=1:k
-        % Sk{i}=S{i};
-        % S{i}=invx{i}*(xtx{i}-X{i}'*E{i}+Z{i}+(X{i}'*Y{i}+V{i}-W{i})/mu);
-    % end
-    Sk=S;
     cmu=cell(k,1);
     cmu(1:k)={mu};
+    % update J_i
+    Jk=J;
+    [J, svp, sv]=cellfun(@updateJ,Z,W,cmu,sv,'UniformOutput',false);
+    % update S_i
+    Sk=S;
     S=cellfun(@updateS,invx,xtx,X,E,Z,Y,V,W,cmu,'UniformOutput',false,'ErrorHandler',@errorfun);
-    % t=toc;
-    % fprintf(1,'update S takes %f\n',t);
-    % update Z
-    % tic;
-    for i=1:k
-        Zk{i}=Z{i};
-        F{i}=(J{i}+S{i}-(W{i}+V{i})*mu)/2;
-    end
-    % t=toc;
-    % fprintf(1,'update Z takes %f\n',t);
     % update ZZ
-    % tic;
-    M=cell(k,1);
-    for i=1:k
-        M{i}=reshape(F{i}',1,n*n);
-    end
+    [F]=cellfun(@updateF,J,S,W,V,cmu,'UniformOutput',false);
+    [M]=cellfun(@updateM,F,'UniformOutput',false);
     MM=zeros(k,n*n);
     for i=1:k
         MM(i,:)=M{i};
@@ -153,57 +131,21 @@ while ~convergenced
         Zk{i}=Z{i};
         Z{i}=reshape(ZZ(i,:),n,n)';
     end
-    % t=toc;
-    % fprintf(1,'update ZZ and Z_i takes %f\n',t);
     % update E_i
-    % tic;
-    for i=1:k
-        Ek{i}=E{i};
-        E{i}=l21(X{i}-X{i}*S{i}+Y{i}/mu,lambda/(2*mu));  % bug fixed, parameter should be lambda/(2*mu), not lambda/mu
-    end
-    % t=toc;
-    % fprintf(1,'update E takes %f\n',t);
+    Ek=E;
+    [E]=cellfun(@updateE,X,S,Y,cmu,clambda,'UniformOutput',false);
     
-    % tic;
-    for i=1:k
-       Xc{i}=X{i}-X{i}*S{i}-E{i}; 
-       ZJc{i}=Z{i}-J{i};
-       ZSc{i}=Z{i}-S{i};
-    end
     % check convergence
-    % find the max error in multi views
-    vals=zeros(k,1);
-    for i=1:k
-        vals(i)=norm(Xc{i},'fro')/Xf{i};
-    end
-    changeX=max(vals);
-    for i=1:k
-        vals(i)=norm(ZJc{i},'fro')/Xf{i};
-    end
-    changeZJ=max(vals);
-    for i=1:k
-        vals(i)=norm(ZSc{i},'fro')/Xf{i};
-    end
-    changeZS=max(vals);
-    for i=1:k
-        vals(i)=norm(Zk{i}-Z{i},'fro')/Xf{i};
-    end
-    changeZ=max(vals); 
-    for i=1:k
-        vals(i)=norm(Jk{i}-J{i},'fro')/Xf{i};
-    end
-    changeJ=max(vals);
-    for i=1:k
-        vals(i)=norm(Sk{i}-S{i},'fro')/Xf{i};
-    end
-    changeS=max(vals);
-    for i=1:k
-        vals(i)=norm(Ek{i}-E{i},'fro')/Xf{i};
-    end
-    changeE=max(vals);
+    [Xv,Xc,ZJv,ZJc,ZSv,ZSc,Zc,Jc,Sc,Ec] = cellfun(@caculateTempVars,X,S,E,Z,J,Zk,Jk,Sk,Ek,Xf,'UniformOutput',false);
+    changeX=max([Xv{:}]);
+    changeZJ=max([ZJv{:}]);
+    changeZS=max([ZSv{:}]);
+    changeZ=max([Zc{:}]); 
+    changeJ=max([Jc{:}]);
+    changeS=max([Sc{:}]);
+    changeE=max([Ec{:}]);
     tmp=[changeZ changeJ changeS changeE ];
     gap=mu*max(tmp);
-
     if mod(iter,50)==0
         fprintf(1,'===========================================================================================================\n');
         fprintf(1,'gap between two iteration is %f,mu is %f\n',gap,mu);
@@ -213,7 +155,6 @@ while ~convergenced
         end
         fprintf(1,'\n');
     end
-
     % if changeX <= epsilon && changeZJ <= epsilon && changeZS <= epsilon
     if changeX <= epsilon && gap <=epsilon2 && changeZJ <= epsilon && changeZS <= epsilon
         convergenced=true;
@@ -224,29 +165,81 @@ while ~convergenced
         end
         fprintf(1,'\n');
     end
-
     % update multipliers
-    for i=1:k
-        Y{i}=Y{i}+mu*Xc{i};
-    end
-    for i=1:k
-        W{i}=W{i}+mu*ZJc{i};
-    end
-    for i=1:k
-        V{i}=V{i}+mu*ZSc{i};
-    end
+    [Y]=cellfun(@updateY,Y,cmu,Xc,'UniformOutput',false);
+    [W]=cellfun(@updateW,W,cmu,ZJc,'UniformOutput',false);
+    [V]=cellfun(@updateV,V,cmu,ZSc,'UniformOutput',false);
     % update parameters
     if gap < epsilon2
         mu=min(rho*mu,max_mu);
     end
     iter=iter+1;
-    % t=toc;
-    % fprintf(1,'other things takes %f\n',t);
 end
 toc
+
+
+% Jk{i}=J{i};
+% [JT,svpt,svt]=singular_value_shrinkage_acc(Z{i}+W{i}/mu,1/mu,sv{i});
+% J{i}=JT;
+% svp{i}=svpt;
+% sv{i}=svt;
+function [J, svp, sv] = updateJ(Z,W,mu,sv)
+    [J,svp,sv]=singular_value_shrinkage_acc(Z+W/mu,1/mu,sv);
+
 % S{i}=invx{i}*(xtx{i}-X{i}'*E{i}+Z{i}+(X{i}'*Y{i}+V{i}-W{i})/mu);
 function [S] = updateS(invx,xtx,X,E,Z,Y,V,W,mu)
     S=invx*(xtx-X'*E+Z+(X'*Y+V-W)/mu);
+
+% F{i}=(J{i}+S{i}-(W{i}+V{i})*mu)/2;
+function [F] = updateF(J,S,W,V,mu)
+    F=(J+S-(W+V)*mu)/2;
+
+% M{i}=reshape(F{i}',1,n*n);
+function [M] = updateM(F)
+    n=length(F);
+    M=reshape(F',1,n*n);
+
+% E{i}=l21(X{i}-X{i}*S{i}+Y{i}/mu,lambda/(2*mu));  % bug fixed, parameter should be lambda/(2*mu), not lambda/mu
+function [E] = updateE(X,S,Y,mu,lambda)
+    E=l21(X-X*S+Y/mu,lambda/(2*mu));
+
+% Xc{i}=X{i}-X{i}*S{i}-E{i}; 
+% ZJc{i}=Z{i}-J{i};
+% ZSc{i}=Z{i}-S{i};
+% vals(i)=norm(Xc{i},'fro')/Xf{i};
+% vals(i)=norm(ZJc{i},'fro')/Xf{i};
+% vals(i)=norm(ZSc{i},'fro')/Xf{i};
+
+% vals(i)=norm(Zk{i}-Z{i},'fro')/Xf{i};
+% vals(i)=norm(Jk{i}-J{i},'fro')/Xf{i};
+% vals(i)=norm(Sk{i}-S{i},'fro')/Xf{i};
+% vals(i)=norm(Ek{i}-E{i},'fro')/Xf{i};
+function [Xv,Xc,ZJv,ZJc,ZSv,ZSc,Zc,Jc,Sc,Ec] = caculateTempVars(X,S,E,Z,J,Zk,Jk,Sk,Ek,Xf)
+    Xc=X-X*S-E; 
+    ZJc=Z-J;
+    ZSc=Z-S;
+    Xv=norm(Xc,'fro')/Xf;
+    ZJv=norm(ZJc,'fro')/Xf;
+    ZSv=norm(ZSc,'fro')/Xf;
+
+    Zc=norm(Zk-Z,'fro')/Xf;
+    Jc=norm(Jk-J,'fro')/Xf;
+    Sc=norm(Sk-S,'fro')/Xf;
+    Ec=norm(Ek-E,'fro')/Xf;
+
+
+% Y{i}=Y{i}+mu*Xc{i};
+% W{i}=W{i}+mu*ZJc{i};
+% V{i}=V{i}+mu*ZSc{i};
+
+function [Y] = updateY(Y,mu,Xc)
+    Y=Y+mu*Xc;
+    
+function [W] = updateW(W,mu,ZJc)
+    W=W+mu*ZJc;
+
+function [V] = updateV(V,mu,ZSc)
+    V=V+mu*ZSc;
 
 function result = errorfun(S, varargin)
    warning(S.identifier, S.message);
